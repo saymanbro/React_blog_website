298c0445fdeda02b01e950c7c4da0ac19d8079dc
saymanbro <sayman7588@gmail.com>
2021-01-30 10:49:37 -0800

    little_change

--- src/App.js
+++ src/App.js
@@ -4,18 +4,18 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+
 } from "react-router-dom";
 import Home from './Component/Home/Home/Home';
 import Navbar from './Component/Home/Navbar/Navbar';
-import SecondCompo from './Component/Home/SecondComponent/SecondCompo';
+
 function App() {
   return (
    <>
    <Router>
    <Navbar/>
      <Switch>
-       <Route path='/home'>
+       <Route exact path='/'>
          <Home/>
         
        </Route>
